f7b428e84e36c49f36ef297eca396e8f4cf9fd3d
Alexander M <mylchenko.alexandr@gmail.com>
2026-02-05 20:25:00 +0200

    SPRINT 1
    ✅ 1. Подключение компонента i18n;
    ✅ 1.1. Языковые пакеты: ua, en;
    ✅ 1.2. Константы
    ✅ 1.3. Словари /locales

--- src/core/i18n/index.ts
+++ src/core/i18n/index.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import lEN from "../../locales/en/common.json";
-import lUA from "../../locales/ua/common.json";
+import lEN from "../../locales/en/dictionary.json";
+import lUA from "../../locales/ua/dictionary.json";
 import {I18N, available as LANGUAGES_AVAILABLE } from "../constants";
 
 i18n
@@ -10,13 +10,13 @@ i18n
   .use( initReactI18next )
   .init({
     resources: {
-      [ I18N.EN ]: { common: lEN },
-      [ I18N.UA ]: { common: lUA },
+      [ I18N.EN ]: { dictionary: lEN },
+      [ I18N.UA ]: { dictionary: lUA },
     },
     fallbackLng: I18N.UA,
     supportedLngs: LANGUAGES_AVAILABLE,
-    ns: ["common"],
-    defaultNS: "common",
+    ns: ["dictionary"],
+    defaultNS: "dictionary",
     interpolation: { escapeValue: false },
     detection: {
       order: ["localStorage", "navigator"],
