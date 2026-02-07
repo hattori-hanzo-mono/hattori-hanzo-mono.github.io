29502a08fc64842e8ed1402429fff2bbce6d4b3c
Alexander M <mylchenko.alexandr@gmail.com>
2026-02-07 17:07:53 +0200

    SPRINT 1
    ✅ 3. Динамический/адаптивный layout ;
    ✅ 3.1. Интеграция с разметкой формата markdown / react-markdown ;
    ✅ 3.2. Мультиязычность markdown контента ;

--- src/hooks/data-loader/index.ts
+++ src/hooks/data-loader/index.ts
@@ -4,7 +4,7 @@ import {useTranslation} from "react-i18next";
 export function useDataLoader(): TDataLoaderHook {
     const { i18n } = useTranslation() ;
     async function getResource( id: string ): Promise<string> {
-        const moduleURI: string = `/public/data/${ i18n.language }/${ id }.md?raw` ;
+        const moduleURI: string = `/assets/data/${ i18n.language }/${ id }.md?raw` ;
         const module: any = await import( moduleURI ) ;
         return Promise.resolve( module.default as string );
     }
